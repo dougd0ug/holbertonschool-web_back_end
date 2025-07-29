33719b699a53220ef00113863d68ec1c84bd701e
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 10:52:27 +0200

    sdsqdqs

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
 """
 square with a tuple from str and list
 """
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
