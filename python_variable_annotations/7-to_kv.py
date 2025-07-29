a01d8e1bb2740017c94cbae1d21e6eb358d3342a
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 10:50:40 +0200

    add doc

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from typing import Union, Tuple
 """
-square with a tuple
+square with a tuple from str and list
 """
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    square with a tuple
+    square with a tuple from str and list
     """
     return (k, float(v ** 2))
