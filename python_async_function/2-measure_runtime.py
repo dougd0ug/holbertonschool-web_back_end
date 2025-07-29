585d489ee3a368537b89e1e3b46af8b6c4288e0e
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 16:17:47 +0200

    add run instead of create task

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,6 +4,7 @@ async chrono
 """
 import asyncio
 import time
+from typing import Callable
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
@@ -13,6 +14,6 @@ async def measure_time(n: int, max_delay: int) -> float:
     measure time
     """
     start = time.perf_counter()
-    asyncio.create_task(wait_n(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
     total_time = (time.perf_counter() - start)
     return (total_time / n)
