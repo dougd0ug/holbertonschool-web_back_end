c82451c352f42e2c3088cf63fc938e33bd6179d3
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 10:33:06 +0200

    return sum of mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+list of floats
+"""
+from typing import List
+
+
+def sum_list(mxd_list: List[float | int]) -> float:
+    """
+    return sum of values in a list
+    """
+    return sum(mxd_list)
