37e596db3dafe3da3f49f59a11e23cabeededa4c
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 09:45:35 +0200

    floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from math import floor as math_floor
+"""
+floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    floor of a float
+    """
+    return math_floor(n)
