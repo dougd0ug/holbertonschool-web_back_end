9bb139b46d322c4c7bae2a9695a1cc48abb06249
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 16:15:30 +0200

    measure time for an execution

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+async chrono
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    measure time
+    """
+    start = time.perf_counter()
+    await asyncio.create_task(wait_n(n, max_delay))
+    total_time = (time.perf_counter() - start)
+    return (total_time / n)
