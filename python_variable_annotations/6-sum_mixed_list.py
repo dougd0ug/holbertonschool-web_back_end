8d11bdcddcf6dc7af302e34d63a586d2a21cca89
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 10:36:06 +0200

    add Union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """
 list of floats
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_list(mxd_lst: list[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     return sum of values in a list
     """
