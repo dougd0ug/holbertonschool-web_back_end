729a9fdcffd6d595fb4a9188c9847bf8d5b3ccb2
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 16:01:27 +0200

    add create_task

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@ async chrono
 """
 import asyncio
 from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -12,7 +13,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     n x wait_random
     """
     results = []
-    task = [asyncio.createtask(wait_random(max_delay)) for _ in range(n)]
+    task = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
     for i in asyncio.as_completed(task):
         result = await i
