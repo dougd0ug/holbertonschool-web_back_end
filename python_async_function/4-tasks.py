5760b71368d7117284ac581f5d441aeffd4a4da7
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 16:26:11 +0200

    delete create task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -13,7 +13,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     n x wait_random
     """
     results = []
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
 
     for task in asyncio.as_completed(tasks):
         result = await task
