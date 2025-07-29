9b6508a37a580c1e875382c4f410b06cf6a15178
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 16:18:56 +0200

    suprr async

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,7 +9,7 @@ from typing import Callable
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     measure time
     """
