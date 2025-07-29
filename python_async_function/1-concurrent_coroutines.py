8607b97f43779b26adf2690d6230d70978a30631
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 15:36:02 +0200

    multi async and sort by ascending

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+async chrono
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    n x wait_random
+    """
+    result = []
+    task = [wait_random(max_delay) for _ in range(n)]
+
+    for i in asyncio.as_completed(task):
+        result = await i
+        result.append(result)
+
+    return result
