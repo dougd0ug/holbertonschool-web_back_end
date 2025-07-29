36fb48ef051ad51448b58b87f17e93bcd58e116a
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 10:34:56 +0200

    change name

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,8 +5,8 @@ list of floats
 from typing import List
 
 
-def sum_list(mxd_list: List[float | int]) -> float:
+def sum_list(mxd_lst: list[float | int]) -> float:
     """
     return sum of values in a list
     """
-    return sum(mxd_list)
+    return sum(mxd_lst)
