d1dc985e9a3d2533380d4a6cc423f193a601ad9c
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 10:24:16 +0200

    sum in a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+list of floats
+"""
+
+
+def sum_list(sum_list: list[float]) -> float:
+    for i in sum_list:
+        result = result + sum_list[i]
+    return result
