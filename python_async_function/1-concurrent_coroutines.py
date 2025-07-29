c3276afd17c0148c177336e978f34b9e88617c74
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-29 15:40:20 +0200

    forgot an s

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,11 +11,11 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     n x wait_random
     """
-    result = []
+    results = []
     task = [wait_random(max_delay) for _ in range(n)]
 
     for i in asyncio.as_completed(task):
         result = await i
-        result.append(result)
+        results.append(result)
 
     return result
