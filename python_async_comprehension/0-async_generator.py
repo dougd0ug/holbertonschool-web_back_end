e325a9dda40ee13000910b2c656ec28dc9b90a58
dougd0ug <dorine.sgv@hotmail.fr>
2025-08-01 14:16:31 +0200

    try with generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,17 @@
 """
 create a rancom loop 10 times
 """
+from typing import Generator, List
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     create a random loop 10 times
     """
+    i = float
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        i = random.uniform(0, 10)
+        yield i
