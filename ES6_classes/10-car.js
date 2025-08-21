0342031d470048fffee062c12ca8017c7b43a1e4
dougd0ug <dorine.sgv@hotmail.fr>
2025-08-21 09:19:33 +0200

    clone car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
+      throw new TypeError('Must be a string');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
