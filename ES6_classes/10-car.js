83f5acf126b5a58acc47aaf934e2e6612b88eb02
dougd0ug <dorine.sgv@hotmail.fr>
2025-08-21 09:47:11 +0200

    add a static method

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -8,6 +8,10 @@ export default class Car {
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
   }
