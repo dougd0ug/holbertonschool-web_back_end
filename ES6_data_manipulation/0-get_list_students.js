a384cd9cf9953f3ac2277863c616bcdc45956a6a
dougd0ug <dorine.sgv@hotmail.fr>
2025-08-25 11:00:13 +0200

    return list of students, array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const result1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const result2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const result3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [result1, result2, result3];
+}
