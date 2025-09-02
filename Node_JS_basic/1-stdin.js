a51784ef6b384fddab30652e96e508b1ca098a8b
dougd0ug <dorine.sgv@hotmail.fr>
2025-09-02 10:00:55 +0200

    create a program and use process to recup stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log('Welcome to Holberton School, what is your name?\n');
+process.stdin.resume();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
