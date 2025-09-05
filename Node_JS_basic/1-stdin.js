7f015fd11835f68071bc3f1420751495b15f21cf
dougd0ug <dorine.sgv@hotmail.fr>
2025-09-05 11:28:50 +0200

    resxcript task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,4 @@
-const readline = require('node:readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-});
-
-rl.on('close', () => {
-  if (!process.stdin.setRawMode) {
-    console.log('This important software is now closing');
-  }
+process.stdout.write('Welcome to Holberton School, what is your name?\n', () => {
+  process.stdin.on('data', (name) => process.stdout.write(`Your name is: ${name}`));
+  process.stdin.on('end', () => process.stdout.write('This important software is now closing\n'));
 });
